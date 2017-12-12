7cfcd9b55b94d62d2e34fbf98b3b177db7726d96
zismano <zismanofir@gmail.com>
2017-12-12 10:18:17 -0800

    Work temporary with exampleDataVideo

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -8,13 +8,13 @@ var VideoListView = Backbone.View.extend({
 
   render: function() {
     this.$el.children().detach();
-    console.log(this.$el)
+    console.log(this.$el);
     this.$el.html(this.template());  
     this.$el.find('div').empty(); 
     this.$el.find('.video-list').append(window.exampleVideoData.map(function(video) {
       console.log(video);
       var newVid = new VideoListEntryView({model: video});
-      return newVid.render()
+      return newVid.render();
     }));
     return this.$el;
   },
